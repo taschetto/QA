01166ef491caa5a06abcd7453018dd8c2f580f2c
Guilherme Taschetto <guilherme@UbuntuGT.(none)>
2013-09-10 18:44:42 -0300

    Corrigido bug de não gravar o nome ao criar uma conta.

--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,5 +1,8 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
+    def sign_up
+    	default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
     def account_update
         default_params.permit(:first_name, :last_name, :about, :birthday, :email, :password, :password_confirmation, :current_password)
     end
