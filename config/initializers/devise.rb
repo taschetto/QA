28078258e03a39f7f1dcf5282f25a0f8d810a377
Guilherme Taschetto <guilherme@ubuntu-guilherme.(none)>
2013-09-23 11:46:25 -0300

    Adicionado :confirmable no :user e mailer configurado

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "gtaschetto@gmail.com"
+  config.mailer_sender = "qapucrs@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
@@ -114,7 +114,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed new email is stored in
   # unconfirmed email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [ :email ]
@@ -137,7 +137,8 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\b[A-Z0-9._%a-z\-]+#{QA::Application.config.email_postfix}/ # /\A[^@]+@[^@]+\z/
+  #config.email_regexp = /\b[A-Z0-9._%a-z\-]+#{QA::Application.config.email_postfix}/ # /\A[^@]+@[^@]+\z/
+  config.email_regexp = /\A[^@]+@[^@]+\z/
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
