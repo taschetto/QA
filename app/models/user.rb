0646bfa84d73372c46c5d59bec4c6f500a222e9e
Guilherme Taschetto <gtaschetto@gmail.com>
2013-09-23 11:46:25 -0300

    Adicionado :confirmable no :user e mailer configurado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ QA::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "qapucrs@gmail.com",
+    password: "gmptsistemas"
+  }  
+
+  config.action_mailer.default_url_options = { :host => 'https://qapucrs.herokuapp.com' }  
 end
