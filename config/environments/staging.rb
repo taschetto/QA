bcb3f938caadf4cd4e52ed1aa8ac0b30cf56e8b4
Guilherme Taschetto <gtaschetto@gmail.com>
2013-09-30 12:54:57 -0300

    Environments tweaking.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ QA::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -14,7 +14,8 @@ QA::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_dispatch.show_exceptions = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
