ac4b275893e92b72a5428f1240e65b0737d2fb1a
Guilherme Taschetto <gtaschetto@gmail.com>
2013-10-14 10:56:35 -0300

    Criar Pergunta: validação extra.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,11 @@ class Question < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
 
-  validate :valid_user, on: :create
+  validate :valid_question, on: :create
  
-  def valid_user
+  def valid_question
     errors[:user_id] << "Você deve estar logado para realizar esta operação." if user_id.nil?
+    errors[:title] << "inválido." if title.empty?
+    errors[:description] << "inválida." if description.empty?
   end  
 end
